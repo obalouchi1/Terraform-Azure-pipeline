ecdbc45182b7db8913fdb75656a89850fc8caaf2
obalouchi1 <51837419+obalouchi1@users.noreply.github.com>
2026-05-20 11:36:14 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.7.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     azurerm = {
@@ -10,7 +10,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate12345"   # replace with your storage account name
+    storage_account_name = "tfstate12345"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
